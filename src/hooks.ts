8fbef52d2bc50ed168941ec97f3cc966a026fa93
Mark Wiltshire <mark.wiltshire88@gmail.coml>
2020-04-27 23:26:25 +0200

    ssr theme

--- src/hooks.ts
+++ src/hooks.ts
@@ -1,16 +1,27 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
+
+declare global {
+  interface Window {
+    __theme: 'light' | 'dark';
+    __onThemeChange: () => void;
+  }
+}
 
 export const useThemeVariation = (): [
   'light' | 'dark',
   (variation: 'light' | 'dark') => void
 ] => {
-  const [variation, setVariation] = useState<'light' | 'dark'>(
-    (window as any).__theme || 'light'
-  );
+  const [variation, setVariation] = useState<'light' | 'dark'>('light');
+  useEffect(() => {
+    setVariation(window.__theme);
+    window.__onThemeChange = () => setVariation(window.__theme);
+  }, []);
   return [
     variation,
     (variation: 'light' | 'dark') => {
       localStorage.setItem('__colorsberlin_theme', variation);
+      document.body.className = variation;
+      window.__theme = variation;
       setVariation(variation);
     }
   ];
