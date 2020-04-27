de3b5755d7a9d0e9df09bad6ac5a70a757e170f5
Mark Wiltshire <mark.wiltshire88@gmail.coml>
2020-04-27 23:24:37 +0200

    ssr theme workaround / focus-visible polyfill

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,10 +1,18 @@
+import 'focus-visible';
+
 export const onClientEntry = () => {
   const preferredTheme = localStorage.getItem('__colorsberlin_theme');
+  window.__setTheme = variation => (document.body.className = variation);
   if (preferredTheme) {
     window.__theme = preferredTheme;
+    document.body.className = preferredTheme;
   } else {
-    window.__theme = window.matchMedia('(prefers-color-scheme: dark)')
-      ? 'dark'
-      : 'light';
+    const darkQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    window.__theme = darkQuery.matches ? 'dark' : 'light';
+    document.body.className = window.__theme;
+    darkQuery.addListener(e => {
+      window.__theme = e.matches ? 'dark' : 'light';
+      window.__onThemeChange();
+    });
   }
 };
