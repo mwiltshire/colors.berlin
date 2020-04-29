12126b866df33e156761512f77971da08c39b37c
Mark Wiltshire <mark.wiltshire88@gmail.coml>
2020-04-29 23:16:17 +0200

    custom 404

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,14 +1,38 @@
-import React from 'react';
-
-import Layout from '../components/layout';
+import React, { FC } from 'react';
+import { graphql } from 'gatsby';
 import SEO from '../components/seo';
+import PalettePost from '../templates/palette-post';
 
-const NotFoundPage = () => (
-  <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
-  </Layout>
+type NotFoundProps = {
+  data: {
+    palette: {
+      colors: string[];
+      district: string;
+      name: string;
+      plz: string;
+      slug: string;
+    };
+  };
+};
+
+export const query = graphql`
+  query NotFoundQuery {
+    palette: palettesJson(name: { eq: "Sterni" }) {
+      colors
+      district
+      name
+      plz
+      slug
+    }
+  }
+`;
+
+const NotFoundPage: FC<NotFoundProps> = ({ data }) => (
+  <>
+    <SEO title="404: Nope" />
+    {/* Let's show a Sterni palette, why the hell not */}
+    <PalettePost data={data} />
+  </>
 );
 
 export default NotFoundPage;
