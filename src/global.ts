3ae6d3b33fb79f7b312e0e79f86c4c5cdb943c90
Mark Wiltshire <mark.wiltshire88@gmail.coml>
2020-05-01 00:30:31 +0200

    global styles

--- src/global.ts
+++ src/global.ts
@@ -1,5 +1,5 @@
 import { css } from '@emotion/core';
-import { Theme } from './theme';
+import { Theme, light, dark } from './theme';
 
 export const makeGlobalStyles = (theme: Theme) => css`
   .js-focus-visible :focus:not(.focus-visible) {
@@ -7,6 +7,7 @@ export const makeGlobalStyles = (theme: Theme) => css`
   }
   html {
     font-size: 16px;
+    line-height: 1.45em;
     -ms-text-size-adjust: 100%;
     -webkit-text-size-adjust: 100%;
   }
@@ -23,12 +24,12 @@ export const makeGlobalStyles = (theme: Theme) => css`
     -webkit-font-feature-settings: 'kern', 'liga', 'clig', 'calt';
     font-feature-settings: 'kern', 'liga', 'clig', 'calt';
     &.light {
-      color: black;
-      background-color: #fff6de;
+      color: ${theme.black};
+      background-color: ${light.background};
     }
     &.dark {
-      color: white;
-      background-color: #282828;
+      color: ${theme.white};
+      background-color: ${dark.background};
     }
   }
   article,
@@ -51,13 +52,6 @@ export const makeGlobalStyles = (theme: Theme) => css`
   video {
     display: inline-block;
   }
-  audio:not([controls]) {
-    display: none;
-    height: 0;
-  }
-  progress {
-    vertical-align: baseline;
-  }
   [hidden],
   template {
     display: none;
@@ -70,38 +64,10 @@ export const makeGlobalStyles = (theme: Theme) => css`
   a:hover {
     outline-width: 0;
   }
-  abbr[title] {
-    border-bottom: none;
-    text-decoration: underline;
-    text-decoration: underline dotted;
-  }
   b,
   strong {
     font-weight: inherit;
     font-weight: bolder;
-  }
-  dfn {
-    font-style: italic;
-  }
-  mark {
-    background-color: #ff0;
-    color: #000;
-  }
-  small {
-    font-size: 80%;
-  }
-  sub,
-  sup {
-    font-size: 75%;
-    line-height: 0;
-    position: relative;
-    vertical-align: baseline;
-  }
-  sub {
-    bottom: -0.25em;
-  }
-  sup {
-    top: -0.5em;
   }
   img {
     border-style: none;
@@ -109,39 +75,10 @@ export const makeGlobalStyles = (theme: Theme) => css`
   svg:not(:root) {
     overflow: hidden;
   }
-  code,
-  kbd,
-  pre,
-  samp {
-    font-family: monospace, monospace;
-    font-size: 1em;
-  }
-  figure {
-    margin: 1em 40px;
-  }
   hr {
     box-sizing: content-box;
     height: 0;
     overflow: visible;
-  }
-  button,
-  input,
-  optgroup,
-  select,
-  textarea {
-    font: inherit;
-    margin: 0;
-  }
-  optgroup {
-    font-weight: 700;
-  }
-  button,
-  input {
-    overflow: visible;
-  }
-  button,
-  select {
-    text-transform: none;
   }
   [type='reset'],
   [type='submit'],
@@ -161,22 +98,6 @@ export const makeGlobalStyles = (theme: Theme) => css`
   [type='submit']:-moz-focusring,
   button:-moz-focusring {
     outline: 1px dotted ButtonText;
-  }
-  fieldset {
-    border: 1px solid silver;
-    margin: 0 2px;
-    padding: 0.35em 0.625em 0.75em;
-  }
-  legend {
-    box-sizing: border-box;
-    color: inherit;
-    display: table;
-    max-width: 100%;
-    padding: 0;
-    white-space: normal;
-  }
-  textarea {
-    overflow: auto;
   }
   [type='checkbox'],
   [type='radio'] {
@@ -199,12 +120,7 @@ export const makeGlobalStyles = (theme: Theme) => css`
     color: inherit;
     opacity: 0.54;
   }
-  ::-webkit-file-upload-button {
-    -webkit-appearance: button;
-    font: inherit;
-  }
   html {
-    font: 112.5%/1.45em georgia, serif;
     box-sizing: border-box;
     overflow-y: scroll;
   }
@@ -251,8 +167,6 @@ export const makeGlobalStyles = (theme: Theme) => css`
     padding-top: 0;
     margin-bottom: 1.45rem;
     color: inherit;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     font-weight: bold;
     text-rendering: optimizeLegibility;
     font-size: 1.62671rem;
@@ -268,8 +182,6 @@ export const makeGlobalStyles = (theme: Theme) => css`
     padding-top: 0;
     margin-bottom: 1.45rem;
     color: inherit;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     font-weight: bold;
     text-rendering: optimizeLegibility;
     font-size: 1.38316rem;
@@ -285,8 +197,6 @@ export const makeGlobalStyles = (theme: Theme) => css`
     padding-top: 0;
     margin-bottom: 1.45rem;
     color: inherit;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     font-weight: bold;
     text-rendering: optimizeLegibility;
     font-size: 1rem;
@@ -302,8 +212,6 @@ export const makeGlobalStyles = (theme: Theme) => css`
     padding-top: 0;
     margin-bottom: 1.45rem;
     color: inherit;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     font-weight: bold;
     text-rendering: optimizeLegibility;
     font-size: 0.85028rem;
@@ -319,22 +227,10 @@ export const makeGlobalStyles = (theme: Theme) => css`
     padding-top: 0;
     margin-bottom: 1.45rem;
     color: inherit;
-    font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto, Oxygen,
-      Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue, sans-serif;
     font-weight: bold;
     text-rendering: optimizeLegibility;
     font-size: 0.78405rem;
     line-height: 1.1;
-  }
-  hgroup {
-    margin-left: 0;
-    margin-right: 0;
-    margin-top: 0;
-    padding-bottom: 0;
-    padding-left: 0;
-    padding-right: 0;
-    padding-top: 0;
-    margin-bottom: 1.45rem;
   }
   ul {
     margin-left: 1.45rem;
@@ -360,74 +256,7 @@ export const makeGlobalStyles = (theme: Theme) => css`
     list-style-position: outside;
     list-style-image: none;
   }
-  dl {
-    margin-left: 0;
-    margin-right: 0;
-    margin-top: 0;
-    padding-bottom: 0;
-    padding-left: 0;
-    padding-right: 0;
-    padding-top: 0;
-    margin-bottom: 1.45rem;
-  }
-  dd {
-    margin-left: 0;
-    margin-right: 0;
-    margin-top: 0;
-    padding-bottom: 0;
-    padding-left: 0;
-    padding-right: 0;
-    padding-top: 0;
-    margin-bottom: 1.45rem;
-  }
   p {
-    margin-left: 0;
-    margin-right: 0;
-    margin-top: 0;
-    padding-bottom: 0;
-    padding-left: 0;
-    padding-right: 0;
-    padding-top: 0;
-    margin-bottom: 1.45rem;
-  }
-  figure {
-    margin-left: 0;
-    margin-right: 0;
-    margin-top: 0;
-    padding-bottom: 0;
-    padding-left: 0;
-    padding-right: 0;
-    padding-top: 0;
-    margin-bottom: 1.45rem;
-  }
-  pre {
-    margin-left: 0;
-    margin-right: 0;
-    margin-top: 0;
-    margin-bottom: 1.45rem;
-    font-size: 0.85rem;
-    line-height: 1.42;
-    background: hsla(0, 0%, 0%, 0.04);
-    border-radius: 3px;
-    overflow: auto;
-    word-wrap: normal;
-    padding: 1.45rem;
-  }
-  table {
-    margin-left: 0;
-    margin-right: 0;
-    margin-top: 0;
-    padding-bottom: 0;
-    padding-left: 0;
-    padding-right: 0;
-    padding-top: 0;
-    margin-bottom: 1.45rem;
-    font-size: 1rem;
-    line-height: 1.45rem;
-    border-collapse: collapse;
-    width: 100%;
-  }
-  fieldset {
     margin-left: 0;
     margin-right: 0;
     margin-top: 0;
@@ -440,59 +269,6 @@ export const makeGlobalStyles = (theme: Theme) => css`
   blockquote {
     margin-left: 1.45rem;
     margin-right: 1.45rem;
-    margin-top: 0;
-    padding-bottom: 0;
-    padding-left: 0;
-    padding-right: 0;
-    padding-top: 0;
-    margin-bottom: 1.45rem;
-  }
-  form {
-    margin-left: 0;
-    margin-right: 0;
-    margin-top: 0;
-    padding-bottom: 0;
-    padding-left: 0;
-    padding-right: 0;
-    padding-top: 0;
-    margin-bottom: 1.45rem;
-  }
-  noscript {
-    margin-left: 0;
-    margin-right: 0;
-    margin-top: 0;
-    padding-bottom: 0;
-    padding-left: 0;
-    padding-right: 0;
-    padding-top: 0;
-    margin-bottom: 1.45rem;
-  }
-  iframe {
-    margin-left: 0;
-    margin-right: 0;
-    margin-top: 0;
-    padding-bottom: 0;
-    padding-left: 0;
-    padding-right: 0;
-    padding-top: 0;
-    margin-bottom: 1.45rem;
-  }
-  hr {
-    margin-left: 0;
-    margin-right: 0;
-    margin-top: 0;
-    padding-bottom: 0;
-    padding-left: 0;
-    padding-right: 0;
-    padding-top: 0;
-    margin-bottom: calc(1.45rem - 1px);
-    background: hsla(0, 0%, 0%, 0.2);
-    border: none;
-    height: 1px;
-  }
-  address {
-    margin-left: 0;
-    margin-right: 0;
     margin-top: 0;
     padding-bottom: 0;
     padding-left: 0;
@@ -567,61 +343,5 @@ export const makeGlobalStyles = (theme: Theme) => css`
     border-bottom: 1px dotted hsla(0, 0%, 0%, 0.5);
     cursor: help;
     text-decoration: none;
-  }
-  thead {
-    text-align: left;
-  }
-  td,
-  th {
-    text-align: left;
-    border-bottom: 1px solid hsla(0, 0%, 0%, 0.12);
-    font-feature-settings: 'tnum';
-    -moz-font-feature-settings: 'tnum';
-    -ms-font-feature-settings: 'tnum';
-    -webkit-font-feature-settings: 'tnum';
-    padding-left: 0.96667rem;
-    padding-right: 0.96667rem;
-    padding-top: 0.725rem;
-    padding-bottom: calc(0.725rem - 1px);
-  }
-  th:first-child,
-  td:first-child {
-    padding-left: 0;
-  }
-  th:last-child,
-  td:last-child {
-    padding-right: 0;
-  }
-  tt,
-  code {
-    background-color: hsla(0, 0%, 0%, 0.04);
-    border-radius: 3px;
-    font-family: 'SFMono-Regular', Consolas, 'Roboto Mono', 'Droid Sans Mono',
-      'Liberation Mono', Menlo, Courier, monospace;
-    padding: 0;
-    padding-top: 0.2em;
-    padding-bottom: 0.2em;
-  }
-  pre code {
-    background: none;
-    line-height: 1.42;
-  }
-  code:before,
-  code:after,
-  tt:before,
-  tt:after {
-    letter-spacing: -0.2em;
-    content: ' ';
-  }
-  pre code:before,
-  pre code:after,
-  pre tt:before,
-  pre tt:after {
-    content: '';
-  }
-  @media only screen and (max-width: 480px) {
-    html {
-      font-size: 100%;
-    }
   }
 `;
